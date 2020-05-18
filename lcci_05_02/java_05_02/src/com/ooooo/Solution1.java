288423aebc725fd3f646f315a8345d66d42e5612
ooooo <1207392850@qq.com>
2020-05-18 10:39:15 +0800

    java_05_02

---
+++ lcci_05_02/java_05_02/src/com/ooooo/Solution1.java
@@ -0,0 +1,30 @@
+package com.ooooo;
+
+class Solution1 {
+  public String printBin(double num) {
+    StringBuilder ans = new StringBuilder("0.");
+    int count = 30;
+    while (num > 0 && count > 0) {
+      num *= 2;
+      if (num >= 1) {
+        ans.append("1");
+        num -= 1;
+      } else {
+        ans.append("0");
+      }
+      count--;
+    }
+    return num != 0 ? "ERROR" : ans.toString();
+  }
+
+  private static void test(double num) {
+    Solution1 solution = new Solution1();
+    String ans = solution.printBin(num);
+    System.out.println(ans);
+  }
+
+  public static void main(String[] args) {
+    test(0.625);
+    test(0.1);
+  }
+}
