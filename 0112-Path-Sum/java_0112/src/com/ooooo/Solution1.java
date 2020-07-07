4473cb8c3130c303fb6c93aed71804b1b8b852da
ooooo <297872913@qq.com>
2020-07-07 20:33:48 +0800

    java_0112

---
+++ 0112-Path-Sum/java_0112/src/com/ooooo/Solution1.java
@@ -0,0 +1,14 @@
+package com.ooooo;
+
+/**
+ * dfs
+ */
+class Solution1 {
+
+  public boolean hasPathSum(TreeNode root, int sum) {
+    if (root == null) return false;
+    if (root.left == null && root.right == null) return sum == root.val;
+    return hasPathSum(root.left, sum - root.val)
+        || hasPathSum(root.right, sum - root.val);
+  }
+}
---
+++ 0112-Path-Sum/java_0112/src/com/ooooo/TreeNode.java
@@ -0,0 +1,12 @@
+package com.ooooo;
+
+public class TreeNode {
+
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+}
