6d299a203fef5d1066c4e2f5d0474c8b046a622c
ooooo <297872913@qq.com>
2020-12-19 18:32:36 +0800

    py_0048

---
+++ 0048-Rotate-Image/py_0048/solution1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/19 18:26
+# @Author  : ooooo
+
+from typing import *
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        for i in range(n // 2):
+            for j in range((n + 1) // 2):
+                matrix[i][j], matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1] \
+                    = matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1], matrix[i][j]
